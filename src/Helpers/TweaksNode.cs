4114ccc7a56f1ae6d78cfe5fbc5d3e380e5986b3
Belim <belim@builtbybel.com>
2022-02-12 19:51:16 +0100

    Add files via upload

---
+++ src/Helpers/TweaksNode.cs
@@ -0,0 +1,18 @@
+﻿using BreakingApp.Tweaks;
+using System.Windows.Forms;
+
+namespace BreakingApp
+{
+    internal class TweaksNode : TreeNode
+    {
+        public TweaksBase Tweak { get; }
+
+        public TweaksNode(TweaksBase tweak)
+        {
+            Tweak = tweak;
+            Text = Tweak.ID();
+            ToolTipText = Tweak.Info();
+            //Checked = true;
+        }
+    }
+}
