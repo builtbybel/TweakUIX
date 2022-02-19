21b5642a05258e9307cb2e5135ebc560e338d3c9
Belim <belim@builtbybel.com>
2022-02-19 16:40:52 +0100

    Add files via upload

---
+++ features/plugins/Enable apps dark mode.ps1
@@ -0,0 +1,13 @@
+﻿###This script will enable app dark mode for all Windows 11 apps supporting this theme mode (including ThisIsWin11).
+# Just some test...
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
+$dir = ls "$PsScriptRoot\..\"
+
+[System.Windows.Forms.MessageBox]::Show("App dark mode has been enabled.`nThisIsWin11 requires a restart for the changes to take effect.","Dark mode",0)
+If (!(Get-Process -Name ThisIsWin11.exe -ErrorAction SilentlyContinue))
+{Stop-Process -Name "ThisIsWin11" -Force
+}
+
+
+
+
